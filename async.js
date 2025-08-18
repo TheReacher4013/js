59f3c1b1dd2ddc4327cbf0fc18c1e14fc49f1e82
TheReacher4013 <rushikeshjoshi4013@gmail.com>
2025-08-18 15:43:30 +0530

    chnages

--- async.js
+++ async.js
@@ -15,3 +15,15 @@ async function photos(){
 }
 console.log("hello Rushikesh");
 photos();
+
+//jar tumhala data web page var hava asel tar tumhi pudhe he karu shakta.
+
+// function renderPhoto(data){
+//     const section = document.getElementById("photos_section");
+//     section.innerHTML = ""
+//     data.forEach(photo => {
+//       const h2 = document.createElement("h2");
+//       h2.innerText = photo.title;
+//       section.appendChild(h2);
+//     });
+// }
